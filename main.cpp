153ac7d90406d52bc86f311c4059ed369d688230
Miguel Angel Bermeo <bermeom@javeriana.edu.co>
2016-09-10 18:20:24 -0400

    Fixed class comunicationChannel

--- main.cpp
+++ main.cpp
@@ -28,11 +28,8 @@ using namespace std;
 std::mutex _lock;
 std::condition_variable_any _cond;
 bool connect_finish = false;
-string id;
-string solve1[10];
-string solve2[10];
-int step_=0;
-bool sw;
+string module_id="gripper_module";
+
 
 
 class connection_listener
@@ -101,9 +98,27 @@ public:
         current_socket = h.socket();
     }
     void subscription(string &module_info){
-            current_socket->on("BLINK", sio::socket::event_listener_aux([&](string const& name, message::ptr const& data, bool isAck,message::list &ack_resp){
+            current_socket->on("SUBSCRIPTION-REPLY", sio::socket::event_listener_aux([&](string const& name, message::ptr const& data, bool isAck,message::list &ack_resp){
                 _lock.lock();
-                cout<<"BLINK"<<endl;
+				json dataJSON=json::parse((data->get_string()));
+				module_id=dataJSON["ID"];
+                HIGHLIGHT("MODULE SUBSCRIBED \n ID "<<module_id);//;
+				// EM(user<<":"<<message);                
+				_cond.notify_all();
+                _lock.unlock();
+                current_socket->off("login");
+            }));
+            current_socket->emit("SUBSCRIPTION", module_info);
+            _lock.lock();
+            _lock.unlock();
+            //bind_events();
+
+    }
+    void bind_events(){
+			current_socket->on("BLINK", sio::socket::event_listener_aux([&](string const& name, message::ptr const& data, bool isAck,message::list &ack_resp){
+                _lock.lock();
+				json dataJSON=json::parse((data->get_string()));
+                EM("\t BLINK");
 				json reply_info={
 					  {"ID","gripper_module"},
 					  {"STATUS","DONE"},
@@ -116,105 +131,10 @@ public:
                 //id = data->get_map()["id"]->get_string();
                 //bool plural = participants !=1;
                 //HIGHLIGHT("Welcome to TAQUIN \nthere"<<(plural?" are ":"'s ")<< participants<<(plural?" participants":" participant")<<"\n Your ID "<<id);//;
-                _cond.notify_all();
-                _lock.unlock();
-                current_socket->off("login");
+				// EM(user<<":"<<message);                
+		        _lock.unlock();
             }));
-            current_socket->emit("SUBSCRIPTION", module_info);
-            _lock.lock();
-            _lock.unlock();
-            bind_events();
-
-    }
-    void bind_events(){
-        	current_socket->on("new message", sio::socket::event_listener_aux([&](string const& name, message::ptr const& data, bool isAck,message::list &ack_resp)
-                               {
-                                   _lock.lock();
-                                   string user = data->get_map()["username"]->get_string();
-                                   string message = data->get_map()["message"]->get_string();
-                                   EM(user<<":"<<message);
-                                   _lock.unlock();
-                               }));
-
-            current_socket->on("start", sio::socket::event_listener_aux([&](string const& name, message::ptr const& data, bool isAck,message::list &ack_resp)
-                               {
-                                   _lock.lock();
-                                    current_socket->emit("start-request", id);
-                                   _lock.unlock();
-                               }));
-            current_socket->on("get-matriz", sio::socket::event_listener_aux([&](string const& name, message::ptr const& data, bool isAck,message::list &ack_resp)
-                               {
-                                   _lock.lock();
-                                   string n_size=data->get_map()["n_size"]->get_string();
-                                    int n;
-                                    if(n_size.size()==1){
-                                        n=n_size[0]-'0';
-                                    }else{
-                                        n=10;
-                                    }
-                                    cout<<n_size<<" "<<n<<endl;
-                                    string matriz="0 3 2 1";
-                                     if (!sw){
-                                            matriz="1 0 3 2";
-                                    }
-                                    current_socket->emit("get-matriz-reply", matriz );
-                                   _lock.unlock();
-                               }));
-            current_socket->on("solve", sio::socket::event_listener_aux([&](string const& name, message::ptr const& data, bool isAck,message::list &ack_resp)
-                               {
-                                    _lock.lock();
-                                    string matriz=data->get_map()["matriz"]->get_string();
-                                    string id_=data->get_map()["id"]->get_string();
-
-                                    if(id_==id+""){
-                                        cout<<"Solve -> ";
-                                        step_=0;
-                                        cout<<matriz<<endl;
-                                        string s=std::to_string((step_+1));
-                                        if (sw){
-                                            s+=" "+solve1[step_];
-                                            cout<<s<<endl;
-                                        }else{
-                                            s+=" "+solve2[step_];
-                                            cout<<s<<endl;
-                                        }
-                                        current_socket->emit("step",s);
-                                        step_++;
-                                    }
-                                    //*/
-                                    _lock.unlock();
-                               }));
-           current_socket->on("ack-solve", sio::socket::event_listener_aux([&](string const& name, message::ptr const& data, bool isAck,message::list &ack_resp)
-                               {
-                                    _lock.lock();
-                                    //string matriz=data->get_map()["matriz"]->get_string();
-                                    //cout<<matriz<<endl;
-                                    cout<<"ack-solve ";
-                                    string s=std::to_string((step_+1));
-                                    if (sw && step_<3){
-                                        s+=" "+solve1[step_];
-                                        cout<<"==> "<<s<<endl;
-                                        current_socket->emit("step",s);
-                                        step_++;
-                                    }else if(!sw && step_<6){
-                                        s+=" "+solve2[step_];
-                                        cout<<"==> "<<s<<endl;
-                                        current_socket->emit("step",s);
-                                        step_++;
-                                    }else{
-                                        step_=0;
-                                    }
-                                    _lock.unlock();
-                               }));
-           current_socket->on("error-solve", sio::socket::event_listener_aux([&](string const& name, message::ptr const& data, bool isAck,message::list &ack_resp)
-                               {
-                                    _lock.lock();
-                                    //string matriz=data->get_map()["matriz"]->get_string();
-                                    //cout<<matriz<<endl;
-                                    //current_socket->emit("step", "0 0");
-                                    _lock.unlock();
-                               }));
-
+        	
 
         }
 
@@ -233,7 +153,8 @@ public:
     void endConnection(){
       h.sync_close();
       h.clear_con_listeners();
-
+	  current_socket->off_all();
+      current_socket->off_error();
     }
 
     ~CommunicationChannel(){
@@ -252,7 +173,7 @@ int main(int argc ,const char* args[])
 {
 
     json module_info={
-      {"ID","gripper_module"},
+      {"ID",module_id},
       {"COMMANDS",json::array({
             {
               {"COMMAND","BLINK"},
@@ -263,31 +184,14 @@ int main(int argc ,const char* args[])
     };
     //{"COMMANDS":[["COMMAND","BLINK"],["PARAMS",[""]]],"ID":"gripper_module"}
     //{"ID":"audiovisual_module","COMMANDS":[{"COMMAND":"BLINK","PARAMS":[]}]}
-    solve1[0]="0 0";
-    solve1[1]="0 1";
-    solve1[2]="1 1";
-
-    solve2[0]="0 1";
-    solve2[1]="1 1";
-    solve2[2]="1 0";
-    solve2[3]="0 0";
-    solve2[4]="0 1";
-    solve2[5]="1 1";
-    //solve2[1]="2 1 0"; "0 3 2 1"; 1 0 3 2
-    //solve2[2]="3 0 0";
+    
     CommunicationChannel st("ws://10.220.16.18",9090);
 	//CommunicationChannel st("ws://localhost",9090);	
         
 	st.start();
-    /*
-    string nickname;
-    while (nickname.length() == 0) {
-        HIGHLIGHT("Type your nickname:");
-        getline(cin, nickname);
-    }*/
     string info=module_info.dump();
     st.subscription(info);
-	  current_socket=st.getCurrent_socket();
+	current_socket=st.getCurrent_socket();
     HIGHLIGHT("Start to Gripper Module\n'$exit' : exit chat\n");
     for (std::string line; std::getline(std::cin, line);) {
         if(line.length()>0)
@@ -296,11 +200,10 @@ int main(int argc ,const char* args[])
             {
                 break;
             }
+			_lock.lock();
+			_lock.unlock();
 
-            current_socket->emit("new message", line);
-            _lock.lock();
-            EM("\t\t\t"<<line<<":"<<"You");
-            _lock.unlock();
+
         }
     }
 
