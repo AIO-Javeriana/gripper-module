5e4ec161c7d55ac1607b6887228b6965edc51bf5
Miguel Angel Bermeo <bermeom@javeriana.edu.co>
2016-10-30 14:31:24 -0400

    working follow line

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include <Module.cpp>
 #include <CommunicationChannel.cpp>
 #include <string>
@@ -23,7 +24,10 @@ using namespace std;
 #define M2DIR 21
 // This is the maximum speed the motors will be allowed to turn.
 // (400 lets the motors go at top speed; decrease to impose a speed limit)
-#define MAX_SPEED  150
+#define MAX_SPEED  200
+#define MIN_SPEED  -200
+#define CALIBRATION_SPEED 150
+#define THRESHOLD 800
 void testADC(){
 
 	int data;
@@ -36,17 +40,30 @@ void testADC(){
 
 }
 
+bool isLine(unsigned int sensorValue){
+	return sensorValue > THRESHOLD;
+}
 
-int loop(ZumoReflectanceSensorArray reflectanceSensors,int lastError)
+bool isThereIntersection(unsigned int sensorValues[]){
+	int sensors=0;
+	int threshold=800;
+	for (int i = 0; i < NUM_SENSORS; i++){
+			cout<<sensorValues[i]<<endl;
+		 if (isLine(sensorValues[i])){
+				sensors++;
+			}else if (i+1 < NUM_SENSORS && isLine(sensorValues[i+1]) && i-1>=0 && !isLine(sensorValues[i-1]) && sensors >0){
+					return true;				
+				}
+		}
+	cout<<" ->>>>>>>>> "<< sensors<<endl;
+	if (sensors==6){
+		return true;
+	}
+	return false;
+}
+
+void followLine(int position ,int &lastError,int &integral)
 {
-  unsigned int sensorValues[NUM_SENSORS];
-  // Get the position of the line.  Note that we *must* provide the "sensors"
-  // argument to readLine() here, even though we are not interested in the
-  // individual sensor readings
-  int position = reflectanceSensors.readLine(sensorValues);
-
-  // Our "error" is how far we are away from the center of the line, which
-  // corresponds to position 2500.
   int error = position - 2500;
 
   // Get motor speed difference using proportional and derivative PID terms
@@ -55,36 +72,143 @@ int loop(ZumoReflectanceSensorArray reflectanceSensors,int lastError)
   // constant of 6, which should work decently for many Zumo motor choices.
   // You probably want to use trial and error to tune these constants for
   // your particular Zumo and line course.
-  int speedDifference = error / 4 + 6 * (error - lastError);
+  //int speedDifference = error / 4 + 6 * (error - lastError);
+  	
+  int speedDifference = error/4  ;+ 6 * (error - lastError)+(integral+error)/8;	
 
+  integral=integral+error;
   lastError = error;
 
   // Get individual motor speeds.  The sign of speedDifference
   // determines if the robot turns left or right.
+  
   int m1Speed = MAX_SPEED + speedDifference;
   int m2Speed = MAX_SPEED - speedDifference;
-
+  //*/
+  /*  
+  int m1Speed =  speedDifference;
+  int m2Speed =  -speedDifference;
+  //*/
+  cout<<" m1Speed "<<m1Speed<<" m2Speed "<<m2Speed<<" error "<< error <<" speedDifference "<<speedDifference<<" integral "<<integral <<endl;
   // Here we constrain our motor speeds to be between 0 and MAX_SPEED.
   // Generally speaking, one motor will always be turning at MAX_SPEED
   // and the other will be at MAX_SPEED-|speedDifference| if that is positive,
   // else it will be stationary.  For some applications, you might want to
   // allow the motor speed to go negative so that it can spin in reverse.
-  if (m1Speed < 0)
-    m1Speed = 0;
-  if (m2Speed < 0)
-    m2Speed = 0;
+  if (m1Speed < MIN_SPEED  )
+    m1Speed = MIN_SPEED ;
+  if (m2Speed < MIN_SPEED)
+    m2Speed = MIN_SPEED;
   if (m1Speed > MAX_SPEED)
     m1Speed = MAX_SPEED;
   if (m2Speed > MAX_SPEED)
     m2Speed = MAX_SPEED;
 
   ZumoMotors::setSpeeds(m1Speed, m2Speed);
-  return lastError;
+  
 }
 
 
 
-void setup(ZumoReflectanceSensorArray reflectanceSensors)
+void move(ZumoReflectanceSensorArray &reflectanceSensors,int &lastError,int &integral,bool &isIntersection,int &direction)
+{
+  unsigned int sensorValues[NUM_SENSORS];
+  // Get the position of the line.  Note that we *must* provide the "sensors"
+  // argument to readLine() here, even though we are not interested in the
+  // individual sensor readings
+  int position = reflectanceSensors.readLine(sensorValues);
+  isIntersection=isThereIntersection(sensorValues);
+  if (isIntersection){
+	cout<<"New Values position "<<position<<endl;
+	for (int i = 0; i < NUM_SENSORS; i++){
+			cout<<sensorValues[i]<<endl;
+		}
+	 return;																															
+	}
+	if (position>2000 && position<3000 &&isLine(sensorValues[2]) && isLine(sensorValues[3])&& !isLine(sensorValues[1])){
+		direction=0;
+	}
+
+	switch (direction){
+		case (1): 
+					position=5000;				
+					for(int i=NUM_SENSORS-1;i>=0;i++){
+						if(isLine(sensorValues[i])){
+							position=i*1000;
+							break;						
+						}
+					}
+			
+					break;
+	};
+	followLine(position ,lastError,integral);
+}
+
+void automaticSensorCalibration(ZumoReflectanceSensorArray &reflectanceSensors){
+ ZumoMotors::init();
+ // Initialize the reflectance sensors module
+  reflectanceSensors.init();
+
+  // Wait for the user button to be pressed and released
+  //button.waitForButton();
+// Wait 1 second and then begin automatic sensor calibration
+  // by rotating in place to sweep the sensors over the line
+  delay(1000);
+  
+  int i; 
+  unsigned long startTime = millis();
+  unsigned long timeLimit=800;
+  for(i = 0; i < 2; i++)
+  {
+	startTime = millis();
+ 	ZumoMotors::setSpeeds(-CALIBRATION_SPEED , CALIBRATION_SPEED*1.05 );	
+	while(millis() - startTime < timeLimit)   // make the calibration take 10 seconds
+	  {
+		reflectanceSensors.calibrate();
+		//delay(10);
+	  }
+  	 ZumoMotors::setSpeeds(0, 0);
+	 startTime = millis();
+     ZumoMotors::setSpeeds(CALIBRATION_SPEED , -CALIBRATION_SPEED*1.05 );
+	while(millis() - startTime < timeLimit)   // make the calibration take 10 seconds
+	  {
+		reflectanceSensors.calibrate();
+	  }
+	ZumoMotors::setSpeeds(0, 0);
+	startTime = millis();
+ 	ZumoMotors::setSpeeds(CALIBRATION_SPEED , -CALIBRATION_SPEED*1.05 );	
+	while(millis() - startTime < timeLimit)   // make the calibration take 10 seconds
+	  {
+		reflectanceSensors.calibrate();
+	  }
+  	 ZumoMotors::setSpeeds(0, 0);
+	 startTime = millis();
+     ZumoMotors::setSpeeds(-CALIBRATION_SPEED , CALIBRATION_SPEED*1.05 );
+	while(millis() - startTime < timeLimit)   // make the calibration take 10 seconds
+	  {
+		reflectanceSensors.calibrate();
+	  }
+    // Since our counter runs to 80, the total delay will be
+    // 80*20 = 1600 ms.
+    
+  }
+  ZumoMotors::setSpeeds(0,0);
+}
+
+
+void manualSensorCalibration(ZumoReflectanceSensorArray &reflectanceSensors){
+ 
+  reflectanceSensors.init();
+  unsigned long startTime = millis();
+  while(millis() - startTime < 10000)   // make the calibration take 10 seconds
+  {
+    reflectanceSensors.calibrate();
+  }
+}
+
+
+
+void setup(ZumoReflectanceSensorArray &reflectanceSensors)
 {
   // Play a little welcome song
   //buzzer.play(">g32>>c32");
@@ -93,32 +217,9 @@ void setup(ZumoReflectanceSensorArray reflectanceSensors)
   // Turn on LED to indicate we are in calibration mode
   pinMode (LED, OUTPUT) ;
   digitalWrite (LED, HIGH) ;	// On
-  // Initialize the reflectance sensors module
-  reflectanceSensors.init();
-
-  // Wait for the user button to be pressed and released
-  //button.waitForButton();
-
-  
-
-  // Wait 1 second and then begin automatic sensor calibration
-  // by rotating in place to sweep the sensors over the line
-  delay(1000);
-  int i;
-  for(i = 0; i < 100; i++)
-  {
-    if ((i > 10 && i <= 30) || (i > 50 && i <= 70))
-      ZumoMotors::setSpeeds(-100, 100);
-    else
-      ZumoMotors::setSpeeds(100, -100);
-    reflectanceSensors.calibrate();
-
-    // Since our counter runs to 80, the total delay will be
-    // 80*20 = 1600 ms.
-    delay(20);
-  }
-  ZumoMotors::setSpeeds(0,0);
-
+ 
+  //manualSensorCalibration(reflectanceSensors);
+  automaticSensorCalibration(reflectanceSensors);
   // Turn off LED to indicate we are through with calibration
   digitalWrite (LED, LOW) ;	// Off
   //buzzer.play(">g32>>c32");
@@ -129,10 +230,22 @@ void setup(ZumoReflectanceSensorArray reflectanceSensors)
   // Play music and wait for it to finish before we start driving.
   //buzzer.play("L16 cdegreg4");
   //while(buzzer.isPlaying());
-  int lastError = 0;
-  while(1){
-  	lastError=loop(reflectanceSensors,lastError);
-  }
+  int lastError = 0,integral=0,direction=0,opc;
+  bool isIntersection=false;
+  do{
+	  cout<<" 0 follow line "<<endl;
+	  cout<<" 1 follow line turn riiight"<<endl;
+	  cout<<" 2 follow line turn left"<<endl;
+	  cout<<" 3 exit "<<endl;
+	  cin>>opc;
+      isIntersection=false;
+	  if(opc==3){
+		isIntersection=true;
+	  }	
+	  while(!isIntersection){
+	  	move(reflectanceSensors,lastError,integral,isIntersection,direction);
+	  }
+    }while(opc!=3);
 }
 
 void testReflectanceSensorArray(ZumoReflectanceSensorArray reflectanceSensors){
@@ -174,7 +287,10 @@ void test(){
   //delay(2000); 
   //ZumoMotors::setSpeeds(100, 100);
   setup(reflectanceSensors);
-
+  ZumoMotors::setSpeeds(0,0 );
+  ZumoMotors::init(); 
+  
+ 
 }
 
 
