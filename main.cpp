491e8b70e05672d8ccde17fcddc5e9b9c72f321b
Miguel Angel Bermeo <bermeom@javeriana.edu.co>
2016-11-01 10:18:48 -0500

    Added commands config starting module, for example -n module_name, -p port, -h host

--- main.cpp
+++ main.cpp
@@ -1,33 +1,39 @@
 
 #include <string>
-#include <queue>
-#include <map>
+#include <string.h>
 //-----------------------
 #include <stdio.h>
 #include <MobilityModule.cpp>
-#include <BlinkService.cpp>
-#include <TellService.cpp>
-#include <MoveService.cpp>
-//#include <wiringPi.h>
-
-
-//#include <ZumoReflectanceSensorArray.h>
 
 using namespace std;
 
-void start();
+void start(string module_id,string host,int port);
 
 int main(int argc ,const char* args[])
-{
-  start();
-	return 0;
+{   
+    string module_id="mobility_module";
+    string host="ws://localhost";
+    int port=9090;
+    for (int i=0;i<argc;i++){
+        if (strcmp (args[i],"-h")==0&& i+1<argc){
+            host=args[i+1];
+        }else if (strcmp (args[i],"-n")==0&& i+1<argc){
+                    module_id=(args[i+1]);
+        }else if (strcmp (args[i],"-p")==0&& i+1<argc){
+                    port=stoi(args[i+1]);
+        }
+        
+    }
+    cout<<module_id<<" "<<host<<" "<<port<<endl;
+    start(module_id,host,port);
+    return 0;
 }
 
 
-void start(){
-  string module_id="mobility_module";
+void start(string module_id,string host,int port){
+  
   MobilityModuleInfo moduleInfo(module_id);
-  MobilityModule* mobilityModule = new MobilityModule("ws://localhost", 9090,&moduleInfo);
+  MobilityModule* mobilityModule = new MobilityModule(host, port,&moduleInfo);
   mobilityModule->start();
   while(true);
 }
